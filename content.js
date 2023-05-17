ba10bc014e1ee00859302862dde1d499414f4ad5
Paul Müller <paulkonstantinmueller@gmail.com>
2023-05-17 13:13:21 +0200

    Added feed statistic removal

--- content.js
+++ content.js
@@ -5,14 +5,49 @@ document.addEventListener('DOMContentLoaded', function() {
 
 // Function to hide LinkedIn stats
 function hideLinkedInStats() {
-    // Selectors might change as LinkedIn updates their website
-    var searchAppearances = document.querySelector('.feed-identity-widget-item__stat');
-    var onProfile1 = document.querySelector('.pvs-list__item--three-column');
-    var onProfile2 = document.querySelector('.visually-hidden');
 
+    // Hide the stats in the feed
+    hideFeedStats();
+        
+    // Define the patterns to match
+    var patterns = [
+        /\d+ profile views/,
+        /\d+ post impressions/,
+        /\d+ search appearances/,
+        /Who's viewed your profile \d+/,
+        /Impressions of your post \d+/
+    ];
+
+    // Recursive function to hide matching text nodes
+    function hideMatchingTextNodes(element) {
+        for (var i = 0; i < element.childNodes.length; i++) {
+            var node = element.childNodes[i];
+
+            // If this is a text node...
+            if (node.nodeType === 3) {
+                // Check if it matches any of the patterns
+                var nodeText = node.nodeValue.trim();
+                for (var j = 0; j < patterns.length; j++) {
+                    if (patterns[j].test(nodeText)) {
+                        // If it matches, hide it
+                        node.parentNode.style.display = 'none';
+                        break;
+                    }
+                }
+            } else if (node.nodeType === 1) {
+                // If this is an element node, check its child nodes
+                hideMatchingTextNodes(node);
+            }
+        }
+    }
+
+    // Start at the body and check all nodes
+    hideMatchingTextNodes(document.body);
+}
+
+function hideFeedStats(){
+    var searchAppearances = document.querySelector('.feed-identity-widget-item__stat');
     if (searchAppearances) searchAppearances.style.display = 'none';
-    if (searchAppearances) onProfile1.style.display = 'none';
-    if (searchAppearances) onProfile2.style.display = 'none';
 }
 
 // Create a new MutationObserver instance
@@ -20,4 +55,3 @@ var observer = new MutationObserver(hideLinkedInStats);
 
 // Start observing the document with the configured parameters
 observer.observe(document, { childList: true, subtree: true });
-
